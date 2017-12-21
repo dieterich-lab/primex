a194fd7cb0c8e815c1d2873bcbfe3450dcc44dcd
alexey0308 <alexey.mipt@gmail.com>
2017-12-21 12:42:13 +0100

    transfer from circtools

--- R/design.R
+++ R/design.R
@@ -238,3 +238,40 @@ toGRanges <- function(primers, exons) {
     primers <- primers$primers
   
 }
+primerCoords <- function(coords) {
+  coords <- do.call(
+    rbind,
+    lapply(coords,
+           function(x) {
+             x <- strsplit(x, ",")[[1]]
+             as.numeric(x)
+           }))
+  colnames(coords) <- c("start", "width")
+  as.data.frame(coords)
+}
+
+primersToRanges <- function(res) {
+  lefts <- do.call(IRanges::IRanges, primerCoords(res$PRIMER_LEFT))
+  rights <- do.call(IRanges::IRanges, primerCoords(res$PRIMER_RIGHT))
+  lapply(seq_along(lefts), function(i) {
+    c(lefts[i], rights[i])
+  })
+}
+
+pairToGRanges <- function(pair, seqs) {
+  seqstrand <- as.character(strand(seqs))[1]
+  res <- lapply(
+    list(start = IRanges::start(pair), end = IRanges::end(pair)),
+    selectex:::.toGenome,
+    upExon = seqs[1],
+    downExon = seqs[2],
+    strand = seqstrand
+  )
+  res <- do.call(rbind, Map(range, res$start, res$end))
+  res <- GenomicRanges::GRanges(
+    seqnames = S4Vectors::Rle(
+      unique(GenomicRanges::seqnames(seqs[1])), lengths = 2),
+    IRanges::IRanges(start = res[, 1], end = res[, 2]),
+    strand = S4Vectors::Rle(unique(GenomicRanges::strand(seqs[1])),2)
+  )
+}
