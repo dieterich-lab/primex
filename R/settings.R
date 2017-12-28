7bc91e01a578ce815e29d3e4fce428e084452d1b
alexey0308 <alexey.mipt@gmail.com>
2017-12-28 12:00:00 +0100

    settings

--- R/settings.R
+++ R/settings.R
@@ -17,6 +17,7 @@ magrittr::`%>%`
 #' p3Settings() %>%
 #'   primerSize(min = 18, optimal = 20, max = 18)
 primerSize <- function(settings, min = NULL, optimal = NULL, max = NULL) {
+    stopifnot(min < optimal, optimal < max)
     settings["PRIMER_MIN_SIZE"] <- list(min)
     settings["PRIMER_OPT_SIZE"] <- list(optimal)
     settings["PRIMER_MAX_SIZE"] <- list(max)
@@ -37,14 +38,28 @@ primerSize <- function(settings, min = NULL, optimal = NULL, max = NULL) {
 #' p3Settings() %>%
 #'   primerTm(min = 58, optimal = 60, max = 62)
 primerTm <- function(settings, min = NULL, optimal = NULL, max = NULL) {
+  stopifnot(min < optimal, optimal < max)
   settings["PRIMER_MAX_TM"] <- list(min)
   settings["PRIMER_OPT_TM"] <- list(optimal)
   settings["PRIMER_MIN_TM"] <- list(max)
   settings
 }
 
+#' Set product size 
+#'
+#' @inheritParams primerSize
+#' @param range a vector with two numbers, c(minimal, maximal) product size
+#' (inclusive).
+#'
+#' @return a settings list with updated values
+#' @export
+#'
+#' @examples
+#' p3Settings() %>% productSize(c(100,300))
+#' 
 productSize <- function(settings, range) {
   stopifnot(length(range) != 2) 
+  stopifnot(range[1] < range[2])
   range <- paste(as.integer(range), collapse = "-")
   settings["PRIMER_PRODUCT_SIZE_RANGE"] <- range
   settings
